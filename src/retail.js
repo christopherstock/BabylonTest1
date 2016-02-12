cf4bbe3f7185ecb78c5b3bb5225419136a4ffefe
Christopher Stock <email@christopherstock.de>
2016-02-12 02:02:26 +0100

    Completed demo cherry picking.

---
+++ src/retail.js
@@ -0,0 +1,12 @@
+﻿var demo = {
+    scene: "Retail",
+    incremental: false,
+    binary: false,
+    doNotUseCDN: false,
+    collisions: true,
+    offline: false,
+    forceLocal: true,
+    onload: function () {
+        // Additionnal tuning options
+    }
+};
